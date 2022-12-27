004e38e0311372d9b78cbd6ea4fb9342278a7e18
giuseppebrandi-byui <bra22041@byui.edu>
2022-12-27 08:26:05 +0000

    Fixed bug

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -102,7 +102,7 @@ def print_instructions():
         "The objective of Tic Tac Toe is to get three in a row.",
         "You will play on a three by three game board.",
         "The first player is known as X and the second is O.",
-        "Players alternate placing Xs and Os on the game board until either",
+        "Players alternate placing Xs and Os on the game board until either ",
         "opponent has three in a row or all nine squares are filled.",
         "X always goes first, and if no one has three in a row, the stalemate "
         "is called a tie.",
